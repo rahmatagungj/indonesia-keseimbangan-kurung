52e8202c31f158009f5b8140c74d22f0d44165d8
Ahmad Yaqdhan <ahmadyaqdhan123@gmail.com>
2022-03-10 18:53:59 +0900

    add yaqdhan's solution

---
+++ solutions/yaqdhan/balancedBrackets.js
@@ -0,0 +1,22 @@
+function balancedBrackets(anything) {
+  const openings = ["{", "[", "("];
+  const closings = ["}", "]", ")"];
+  let expected = [];
+
+  for (const letter of anything.split("")) {
+    if (openings.includes(letter)) {
+      expected.push(closings[openings.indexOf(letter)]);
+    } else if (closings.includes(letter)) {
+      if (expected.pop() !== letter) {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+
+  return !expected.length;
+}
+
+// dont change the code below
+exports.balancedBrackets = balancedBrackets;
